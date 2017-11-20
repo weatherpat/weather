2be6f771ae387e4f18ecf9cb955ce9fc9d91f9d7
visionbaby <visionbaby@users.noreply.github.com>
2017-11-20 19:10:50 +0800

    Update text.js

--- js/text.js
+++ js/text.js
@@ -1,4 +1,4 @@
 document.writeln("<center>'可谓“一波未，一波又来”'</center>");
-document.writeln("<center>'还有强冷,我的秋那么快就要结束而要入冬...'</center>");
+document.writeln("<center>'本周末还有强冷影响中南部地区(2017/11/19)...'</center>");
 document.writeln("<center>P.S.'不以下雪为目地的冷空气都是耍流氓'</center>");
 document.writeln("</br>");
