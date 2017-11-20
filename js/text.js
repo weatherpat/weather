aa3315f481fa9cc30fadfa28d1838b90b4686ef7
weatherpat <33831179+weatherpat@users.noreply.github.com>
2017-11-20 21:08:59 +0800

    Update text.js

--- js/text.js
+++ js/text.js
@@ -1,4 +1,4 @@
-document.writeln("<center>'可谓“一波未，一波又来”'</center>");
-document.writeln("<center>'本周末还有强冷影响中南部地区(2017/11/19)'</center>");
-document.writeln("<center>P.S.'不以下雪为目地的冷空气都是耍流氓'</center>");
-document.writeln("</br>");
+document.writeln("<center>可谓“一波未停，一波又来”</center>");
+document.writeln("<center>本周末还有强冷影响中南部地区(2017/11/19)</center>");
+document.writeln("<center>P.S.不以下雪为目地的冷空气都是耍流氓</center>");
+document.writeln("</br>");   // 定义换行
