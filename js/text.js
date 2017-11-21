237ce64a020e204a1708ee55c9706550b863174c
weatherpat <33831179+weatherpat@users.noreply.github.com>
2017-11-21 10:02:45 +0800

    Update text.js

--- js/text.js
+++ js/text.js
@@ -1,3 +1,3 @@
-document.writeln("<center>一波過去,一波漸近,別急還有一波未到</center>");
+document.writeln("<center>一波過去,一波漸近,別急!還有一波未到.</center>");
 document.writeln("<center> 　　　　　　　　 2017/11/21( ┭┮﹏┭┮)</center>");
 document.writeln("</br>");   // 定义换行
