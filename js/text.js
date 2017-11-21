bd6e7196a4373196c1d81bbe62fbdafbe0d27c7e
weatherpat <33831179+weatherpat@users.noreply.github.com>
2017-11-21 09:58:12 +0800

    Update text.js

--- js/text.js
+++ js/text.js
@@ -1,3 +1,3 @@
 document.writeln("<center>一波過去,一波漸近,別急還有一波未到</center>");
-document.writeln("<center> 　　　　　　　　　 2017/11/21( •̥́ ˍ •̀ू )</center>");
+document.writeln("<center> 　　　　　　　　　 2017/11/21( •̥́ ˍ •̀ू)</center>");
 document.writeln("</br>");   // 定义换行
