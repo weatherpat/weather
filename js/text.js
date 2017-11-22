7230e2a049a4c632ed701bdcbe3434a1a8383a58
weatherpat <33831179+weatherpat@users.noreply.github.com>
2017-11-22 15:55:05 +0800

    Update text.js

--- js/text.js
+++ js/text.js
@@ -1,3 +1,3 @@
-document.writeln("<center>小雪小雪,寒氣入侵,囤肉過年....</center>");
-document.writeln("<center> 　　　2017/11/22(廣東:今天小雪?不存在)</center>");
-document.writeln("</br>");   // 定义换行
+document.writeln("小雪小雪,寒氣入侵,囤肉過年....");
+document.writeln("    　　　2017/11/22(廣東:今天小雪?不存在)");
+document.writeln("</br>");   // 保留换行
