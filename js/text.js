bb8453ec52f6e17c47bb72d77a325de054a53f9b
weatherpat <33831179+weatherpat@users.noreply.github.com>
2017-11-22 15:52:41 +0800

    Update text.js

--- js/text.js
+++ js/text.js
@@ -1,3 +1,3 @@
-document.writeln("<center>一波過去,一波漸近,別急!還有一波未到.</center>");
-document.writeln("<center> 　　　　　　　　 2017/11/21( ┭┮﹏┭┮)</center>");
+document.writeln("<center>小雪小雪,寒氣入侵,囤肉過年....</center>");
+document.writeln("<center> 　　　2017/11/22(廣東:今天小雪?不存在)</center>");
 document.writeln("</br>");   // 定义换行
