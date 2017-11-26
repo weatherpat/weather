5243e6415c6d4ee251a9beaa0009215ace8e0e2a
weatherpat <33831179+weatherpat@users.noreply.github.com>
2017-11-26 22:52:39 +0800

    Update text.js

--- js/text.js
+++ js/text.js
@@ -1,10 +1,4 @@
-document.writeln("<a href=\'https://weatherpat.github.io/weather/js/text.js\' target=\'_blank\'>更新日記...</a>"); //查看本文檔記錄
+document.writeln("<a href=\'https://weatherpat.github.io/weather/Diary.txt\' target=\'_blank\'>更新記錄...</a>"); //本文檔歷史記錄
 document.writeln("</br>　　雪人無緣孩童樂，牽手影伴盼炊煙");
 document.writeln("</br>　　　凍寒天气秋日衣，西北凄厉趕枯榮");
 document.writeln("</br></br>"); //保留換行
-// 歷史:
-// 20171126:雪人無緣孩童樂，牽手影伴盼炊煙;凍寒天气秋日衣，西北凄厉趕枯榮
-// 20171124:吵什麼吵,下週暖給你看
-// 20171123:雪花飄呀飄,飄呀飄,飄個大公雞
-// 20171122:小雪小雪,寒氣入侵,囤肉過年
-// 20171120:不以下雪为目地的冷空气都是耍流氓
