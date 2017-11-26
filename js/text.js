c831941ed7509011561d00621ece9968e00850de
weatherpat <33831179+weatherpat@users.noreply.github.com>
2017-11-26 22:20:23 +0800

    Update text.js

--- js/text.js
+++ js/text.js
@@ -1,5 +1,5 @@
-document.writeln("</br><a href=\'https://weatherpat.github.io/weather/js/text.js\' target=\'_blank\'>更新日記...</a>"); //查看本文檔記錄
-document.writeln("　　雪人無緣孩童樂，牽手影伴盼炊煙");
+document.writeln("<a href=\'https://weatherpat.github.io/weather/js/text.js\' target=\'_blank\'>更新日記...</a>"); //查看本文檔記錄
+document.writeln("</br>　　雪人無緣孩童樂，牽手影伴盼炊煙");
 document.writeln("</br>　　　凍寒天气秋日衣，西北凄厉趕枯榮");
 document.writeln("</br></br>"); //保留換行
 // 歷史:
