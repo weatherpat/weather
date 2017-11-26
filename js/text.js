4056f41903c0bde16f8f829dab28722cec7540a5
weatherpat <33831179+weatherpat@users.noreply.github.com>
2017-11-26 21:49:01 +0800

    Update text.js

--- js/text.js
+++ js/text.js
@@ -1,3 +1,3 @@
-document.writeln("　　谁将平地万堆雪，剪刻作此连天花");
+document.writeln("　　雪人無緣孩童樂，伴影牵手望炊烟");
 document.writeln("</br>　　廣東:凍寒天气秋日衣,北風凄厉趕枯榮");
-document.writeln("</br>");   // 保留换行
+document.writeln("</br></br>");   // 保留换行
