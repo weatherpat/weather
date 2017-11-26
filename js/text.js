38c0016b51072ed85fb96857a385500ddb1213c2
weatherpat <33831179+weatherpat@users.noreply.github.com>
2017-11-26 23:44:17 +0800

    Update text.js

--- js/text.js
+++ js/text.js
@@ -1,4 +1,3 @@
-document.writeln("<a href=\'https://weatherpat.github.io/weather/Diary.txt\' target=\'_blank\'>歷史記錄...</a>"); //本文檔歷史記錄
 document.writeln("</br>　　雪人無緣孩童樂，牽手影伴盼炊煙");
 document.writeln("</br>　　　凍寒天气秋日衣，西北凄厉趕枯榮");
 document.writeln("</br></br>"); //保留換行
