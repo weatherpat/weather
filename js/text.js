3976fab6f6ad48d9895995712fe9473f98af41b5
weatherpat <33831179+weatherpat@users.noreply.github.com>
2017-11-28 23:51:51 +0800

    Update text.js

--- js/text.js
+++ js/text.js
@@ -1,3 +1,3 @@
-document.writeln("</br>　　雪人無緣孩童樂，牽手影伴盼炊煙");
-document.writeln("</br>　　　凍寒天气秋日衣，西北凄厉趕枯榮");
+document.writeln("</br>　　極渦副高來相聚,五月南方一月寒;");
+document.writeln("</br>　　　　　　　　　　　冰與火交替....");
 document.writeln("</br></br>"); //保留換行
