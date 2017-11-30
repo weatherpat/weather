eb536f6305de0b8edcef26651d4a48d379893f36
weatherpat <33831179+weatherpat@users.noreply.github.com>
2017-11-30 14:49:54 +0800

    Update text.js

--- js/text.js
+++ js/text.js
@@ -1,3 +1,3 @@
-document.writeln("</br>　　極渦副高來相聚,五月南方一月寒;");
-document.writeln("</br>　　　　　　　　　　　冰與火交替....");
+document.writeln("</br>　　南嶺以北一片藍,南嶺以南一片紅;");
+document.writeln("</br>　　　　　　　　　　　五嶺齊集四季天....");
 document.writeln("</br></br>"); //保留換行
