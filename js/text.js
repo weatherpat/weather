1c000b5ab1ecb92fd7a7b3ba1b117ec9225395b3
weatherpat <33831179+weatherpat@users.noreply.github.com>
2017-11-30 14:50:24 +0800

    Update text.js

--- js/text.js
+++ js/text.js
@@ -1,3 +1,3 @@
 document.writeln("</br>　　南嶺以北一片藍,南嶺以南一片紅;");
-document.writeln("</br>　　　　　　　　　　　五嶺齊集四季天....");
+document.writeln("</br>　　　　　　　　　　　五嶺齊集四季天...");
 document.writeln("</br></br>"); //保留換行
