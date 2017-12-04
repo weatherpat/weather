812244f0fa9f78cac5f7904b1c9d3a7a4996b188
weatherpat <33831179+weatherpat@users.noreply.github.com>
2017-12-04 09:44:23 +0800

    Update gdmo.js

--- js/gdmo.js
+++ js/gdmo.js
@@ -1,1 +1,6 @@
-document.writeln("<div id=\'gdmo_7day\' height=\'400px\' width=\'800px\'><center><iframe style=\'margin-top:-80px;width:800px;display:block;\' src=\'http://www.gdmo.cn/weather-gdmo/gdWeather/gd-weather!gdWeather.action#toplink\' id=\'index_content\' frameborder=\'0\' scrolling=\'no\' height=\'650\' style=\'width:800\'; border: none; float: right; min-height: 400px;\' name=\'content\'></center></iframe></div>");
+document.writeln("<div id=\'gdmo_7day\' height=\'400px\' width=\'800px\'>");
+document.writeln("  <center>");
+document.writeln("   <iframe style=\'margin-top:-80px;width:800px;display:block;\' src=\'http://www.gdmo.cn/weather-gdmo/gdWeather/gd-weather!gdWeather.action#toplink\' id=\'index_content\' frameborder=\'0\' scrolling=\'no\' height=\'650\' style=\'width:800\'; border: none; float: right; min-height: 400px;\' name=\'content\'>");
+document.writeln("   </iframe>");
+document.writeln("  </center>");
+document.writeln("</div>");
