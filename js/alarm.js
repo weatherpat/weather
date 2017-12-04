4490f71f4129d51357f44780f8af2085c4944491
weatherpat <33831179+weatherpat@users.noreply.github.com>
2017-12-04 09:43:00 +0800

    Update alarm.js

--- js/alarm.js
+++ js/alarm.js
@@ -1,1 +1,6 @@
-document.writeln("<div id=\'alarm\' align=\'center\'><center><div style=\'width:1000px;height:100%;overflow-x:hidden;overflow-y:auto;border:0px\' scrolling=\'auto\'><iframe style=\'width:1200px;height:1400px;margin:-200px;margin-left:-484px;display:block;\' src=\'http://www.nmc.cn/f/alarm.html\'  frameborder=\'0\' scrolling=\'no\' ></center></iframe></div></div>");
+<div id='alarm' align='center'>
+   <div style='width:1000px;height:100%;overflow-x:hidden;overflow-y:auto;border:0px' scrolling='auto'>
+     <iframe style='width:1200px;height:1400px;margin:-200px;margin-left:-484px;display:block;' src='http://www.nmc.cn/f/alarm.html'  frameborder='0' scrolling='no' >
+     </iframe>
+   </div>
+</div>
