48b933b2eac80baef2080ac536290a6b48f7c8de
weatherpat <33831179+weatherpat@users.noreply.github.com>
2017-12-04 09:43:17 +0800

    Update alarm.js

--- js/alarm.js
+++ js/alarm.js
@@ -1,6 +1,6 @@
-<div id='alarm' align='center'>
-   <div style='width:1000px;height:100%;overflow-x:hidden;overflow-y:auto;border:0px' scrolling='auto'>
-     <iframe style='width:1200px;height:1400px;margin:-200px;margin-left:-484px;display:block;' src='http://www.nmc.cn/f/alarm.html'  frameborder='0' scrolling='no' >
-     </iframe>
-   </div>
-</div>
+document.writeln("<div id=\'alarm\' align=\'center\'>");
+document.writeln("   <div style=\'width:1000px;height:100%;overflow-x:hidden;overflow-y:auto;border:0px\' scrolling=\'auto\'>");
+document.writeln("     <iframe style=\'width:1200px;height:1400px;margin:-200px;margin-left:-484px;display:block;\' src=\'http://www.nmc.cn/f/alarm.html\'  frameborder=\'0\' scrolling=\'no\' >");
+document.writeln("     </iframe>");
+document.writeln("   </div>");
+document.writeln("</div>");
