cf469d21badbcfa1e18fa50ffc1124eccc566894
weatherpat <33831179+weatherpat@users.noreply.github.com>
2017-12-04 09:41:55 +0800

    Update KMA.js

--- js/KMA.js
+++ js/KMA.js
@@ -1,1 +1,6 @@
-document.writeln("<div id=\'alarm\' align=\'center\'><div style=\'width:900px;height:700px;overflow-x:hidden;overflow-y:auto;border:0px\' scrolling=\'auto\'><iframe style=\'width:1000px;height:1020px;margin:-320px;margin-left:-700px;display:block;\' src=\'http://web.kma.go.kr/chn/weather/typoon/image01.jsp\'  frameborder=\'0\' scrolling=\'no\' ></iframe></div></div>");
+document.writeln("<div id=\'alarm\' align=\'center\'>");
+document.writeln("  <div style=\'width:900px;height:700px;overflow-x:hidden;overflow-y:auto;border:0px\' scrolling=\'auto\'>");
+document.writeln("    <iframe style=\'width:1000px;height:1020px;margin:-320px;margin-left:-700px;display:block;\' src=\'http://web.kma.go.kr/chn/weather/typoon/image01.jsp\'  frameborder=\'0\' scrolling=\'no\' >");
+document.writeln("    </iframe>");
+document.writeln("  </div>");
+document.writeln("</div>");
