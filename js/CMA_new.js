f20c85ac99fc1b17d9e78831204a5811255cbea4
weatherpat <33831179+weatherpat@users.noreply.github.com>
2017-12-04 09:40:31 +0800

    Update CMA_new.js

--- js/CMA_new.js
+++ js/CMA_new.js
@@ -1,1 +1,6 @@
-document.writeln("<div id=\'alarm\' align=\'center\'><div style=\'width:1025px;height:733px;overflow-x:hidden;overflow-y:hidden;border:0px\' scrolling=\'auto\'><iframe style=\'width:1265px;height:925px;margin:-115px;margin-left:-100px;display:block;\' src=\'http://www.weather.com.cn/live/\'  frameborder=\'0\' scrolling=\'no\'></iframe></div></div>");
+document.writeln("<div id=\'alarm\' align=\'center\'>");
+document.writeln("  <div style=\'width:1025px;height:733px;overflow-x:hidden;overflow-y:hidden;border:0px\' scrolling=\'auto\'>");
+document.writeln("    <iframe style=\'width:1265px;height:925px;margin:-115px;margin-left:-100px;display:block;\' src=\'http://www.weather.com.cn/live/\'  frameborder=\'0\' scrolling=\'no\'>");
+document.writeln("      </iframe>");
+document.writeln("  </div>");
+document.writeln("</div>");
