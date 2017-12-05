a1d49b865bed47f2fc809dade5bb2fc923f2154d
weatherpat <33831179+weatherpat@users.noreply.github.com>
2017-12-05 14:39:09 +0800

    Create typhoon.nmc.mob.js

---
+++ js/typhoon.nmc.mob.js
@@ -0,0 +1,4 @@
+document.writeln("<div id=\'typhoon.nmc\' width=\'100%\'>");
+document.writeln(" <center>");
+document.writeln("  <iframe style=\'margin-top:-55px;width:100%;display:block;\' src=\'http://typhoon.nmc.cn/mobile.html\'frameborder=\'0\' scrolling=\'no\' height=\'638\' style=\'width:100%\';></iframe>");
+document.writeln(" </center>");
