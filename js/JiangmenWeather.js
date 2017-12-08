24a2e7fd721b3abce82fbc3582613a96046f570a
visionbaby <visionbaby@users.noreply.github.com>
2017-12-09 02:14:45 +0800

    Update JiangmenWeather.js

--- js/JiangmenWeather.js
+++ js/JiangmenWeather.js
@@ -1,4 +1,4 @@
-document.writeln("<div style=\'width: 475px;\'>");
-document.writeln("  <iframe style=\'overflow:hidden;border:none\' allowtransparency=\'true\' width=\'475\' height=\'205\' src=\'https://zh.weather-forecast.com/locations/Jiangmen/forecasts/latest/threedayfree\' scrolling=\'no\' frameborder=\'0\' marginwidth=\'0\' marginheight=\'0\'>");
+document.writeln("<div style=\'width: 350px;\'>");
+document.writeln("  <iframe style=\'overflow:hidden;border:none\' allowtransparency=\'true\' width=\'350\' height=\'151\' src=\'https://zh.weather-forecast.com/locations/Jiangmen/forecasts/latest/threedayfree\' scrolling=\'no\' frameborder=\'0\' marginwidth=\'0\' marginheight=\'0\'>");
 document.writeln("  </iframe>");
 document.writeln("</div>");
