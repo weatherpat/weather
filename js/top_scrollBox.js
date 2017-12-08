bfb0222eb72ed1dabfd0857b2b74ee860d6c578b
visionbaby <visionbaby@users.noreply.github.com>
2017-12-08 23:07:58 +0800

    Create top_scrollBox.js

---
+++ js/top_scrollBox.js
@@ -0,0 +1,7 @@
+document.writeln("<link rel=\'shortcut icon\' href=\'favicon.ico\' type=\'image/x-icon\'/>");
+document.writeln("<div id=\'scrollBox\'><ul id=\'con1\'></br>");
+document.writeln("<li><a href=\'http://www.cpc.noaa.gov/products/analysis_monitoring/enso_advisory/figure01.gif\'>拉尼娜终究还是要来...</a>");
+document.writeln("<li><a href=\'#\'>往上滚动停顿代码</a>");
+document.writeln("<li><a href=\'#\'>往上滚动停顿代码</a>");
+document.writeln("<li><a href=\'#\'>往上滚动停顿代码</a>");
+document.writeln("</br></ul><ul id=\'con2\'></ul></div>");
