909a4f1413f92296e7367c6f311136fe92b574ad
visionbaby <visionbaby@users.noreply.github.com>
2017-12-09 01:00:08 +0800

    Update text.js

--- js/text.js
+++ js/text.js
@@ -1,3 +1,3 @@
-document.writeln("</br>　　“大雪”氣節，全國“貧雪”;");
-document.writeln("</br>拉呢娜襲擊到來,小伙伴們開啟火墙防禦吧.;");
+document.writeln("</br>　西伯利亞“雞窩”來了.");
+document.writeln("</br>　　"公雞,公雞"哺育的不是您,快跑...");
 document.writeln("</br></br>"); //保留換行
