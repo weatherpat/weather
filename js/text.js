9d165b3f6df2bbecff44b65d99730e83cb750c5f
visionbaby <visionbaby@users.noreply.github.com>
2017-12-09 01:06:44 +0800

    Update text.js

--- js/text.js
+++ js/text.js
@@ -1,3 +1,3 @@
-document.writeln("</br>　西伯利亞“雞窩”來了,");
-document.writeln("</br>　　"公雞,公雞"哺育的不是您,快跑.快跑..");
+document.writeln("</br>　西伯利亞“雞窩”來了,;");
+document.writeln("</br>　　"公雞,公雞"哺育的不是您,快跑.快跑..;");
 document.writeln("</br></br>"); //保留換行
