385e198bd0d0e866307c65b1f5e29853e7e538bb
visionbaby <visionbaby@users.noreply.github.com>
2017-12-09 01:45:06 +0800

    Update scrollBox_content.js

--- js/scrollBox_content.js
+++ js/scrollBox_content.js
@@ -1,6 +1,8 @@
+document.writeln("</br>");
 document.writeln("<div id=\'scrollBox\' style=\'text-align:center\'><ul id=\'con1\'></br>");
 document.writeln("<li><a href=\'https://www.tropicaltidbits.com/analysis/models/?model=ecmwf&region=fe&pkg=T850a\' target=\'_blank\'>● 20171209 西伯利亚“恶魔”来了，十分强大...</a>");
 document.writeln("<li><a href=\'http://mp.weixin.qq.com/s/r7ecuHt9PT2Gms42DH8Aqw\' target=\'_blank\'>● 20171207 “大雪”這天，全國深度“貧雪”.</a>");
 document.writeln("<li><a href=\'http://www.cpc.noaa.gov/products/analysis_monitoring/enso_advisory/figure01.gif\' target=\'_blank\'>● 20171205 拉尼娜终究还是要来了...</a>");
 document.writeln("<li><a href=\'http://www.cma.gov.cn/2011xzt/2017zt/2017_24st/daxue/\' target=\'_blank\'>● 20171201 兩大數值輪流攤大餅, 牡丹江林海童话幻化成真</a>");
 document.writeln("</br></ul><ul id=\'con2\'></ul></div>");
+document.writeln("</br>");
