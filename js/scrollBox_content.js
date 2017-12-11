486b4fe220233683b24b0ba7dc08d27065f3803a
visionbaby <visionbaby@users.noreply.github.com>
2017-12-12 00:34:55 +0800

    Update scrollBox_content.js

--- js/scrollBox_content.js
+++ js/scrollBox_content.js
@@ -1,5 +1,6 @@
 document.writeln("</br>");
 document.writeln("<div id=\'scrollBox\'><ul id=\'con1\'></br>");
+document.writeln("<li><a href=\'http://imgsrc.baidu.com/forum/pic/item/d1d4b9014a90f60397d815c23212b31bb151eda6.jpg\' target=\'_blank\'>20171213 奶死1個是1個.</a>");
 document.writeln("<li><a href=\'https://www.tropicaltidbits.com/analysis/models/?model=ecmwf&region=fe&pkg=T850a\' target=\'_blank\'>20171209 西伯利亚“恶魔”来了.</a>");
 document.writeln("<li><a href=\'http://www.cma.gov.cn/2011xzt/2017zt/2017_24st/daxue/\' target=\'_blank\'>20171207 “大雪”這天,全國深度“貧雪”.</a>");
 document.writeln("<li><a href=\'http://www.cpc.noaa.gov/products/analysis_monitoring/enso_advisory/figure01.gif\' target=\'_blank\'>20171205 拉尼娜终究还是要来的.</a>");
