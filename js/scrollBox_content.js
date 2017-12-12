9c02d6e591ddb9f1d82d2ab8c2d19244c25df5be
visionbaby <visionbaby@users.noreply.github.com>
2017-12-12 12:02:10 +0800

    Update scrollBox_content.js

--- js/scrollBox_content.js
+++ js/scrollBox_content.js
@@ -1,9 +1,9 @@
 document.writeln("</br>");
 document.writeln("<div id=\'scrollBox\'><ul id=\'con1\'></br>");
-document.writeln("<li><a href=\'http://imgsrc.baidu.com/forum/pic/item/d1d4b9014a90f60397d815c23212b31bb151eda6.jpg\' target=\'_blank\'>20171213 奶死1個是1個.</a>");
+document.writeln("<li><a href=\'http://imgsrc.baidu.com/forum/pic/item/d1d4b9014a90f60397d815c23212b31bb151eda6.jpg\' target=\'_blank\'>2017120 奶死1個是1個.</a>");
 document.writeln("<li><a href=\'https://www.tropicaltidbits.com/analysis/models/?model=ecmwf&region=fe&pkg=T850a\' target=\'_blank\'>20171209 西伯利亚“恶魔”来了.</a>");
 document.writeln("<li><a href=\'http://www.cma.gov.cn/2011xzt/2017zt/2017_24st/daxue/\' target=\'_blank\'>20171207 “大雪”這天,全國深度“貧雪”.</a>");
 document.writeln("<li><a href=\'http://www.cpc.noaa.gov/products/analysis_monitoring/enso_advisory/figure01.gif\' target=\'_blank\'>20171205 拉尼娜终究还是要来的.</a>");
-document.writeln("<li><a href=\'http://p.weather.com.cn/2017/12/2806378.shtml\' target=\'_blank\'>20171201 兩大數值畫餅,牡丹江童话成真</a>");
+document.writeln("<li><a href=\'https://commons.wikimedia.org/wiki/File:Guar%C3%A1_2017-12-09_1532Z.jpg\' target=\'_blank\'>20171222 南太的Guará,你還好嗎</a>");
 document.writeln("</br></ul><ul id=\'con2\'></ul></div>");
 document.writeln("</br>");
