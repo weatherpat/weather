dabe70f92602898d14da56419952b06acac2a22f
visionbaby <visionbaby@users.noreply.github.com>
2017-12-21 16:26:14 +0800

    Update text.js

--- js/text.js
+++ js/text.js
@@ -1,3 +1,3 @@
-document.writeln("</br>　雞窩欺負人,物理攻擊北方，魔法攻擊南方");
-document.writeln("</br>　　　　東台刷人頭,願逝者安息...");
+document.writeln("</br>　雞窩妖孽到處搗亂;颱風排隊來觀望");
+document.writeln("</br>　　　　冷空氣:物理攻擊北方，魔法攻擊南方");
 document.writeln("</br></br>"); //保留換行
