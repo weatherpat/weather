50c348fd5b6ffad6ffa66964a25293a944ae7cca
visionbaby <visionbaby@users.noreply.github.com>
2017-12-21 16:22:25 +0800

    Update text.js

--- js/text.js
+++ js/text.js
@@ -1,3 +1,3 @@
-document.writeln("</br>　窮人沒褲穿,雞窩欺負人;");
-document.writeln("</br>　　東台刷人頭,我們默哀,願逝者安息...");
+document.writeln("</br>　雞窩欺負人,物理攻擊北方，魔法攻擊南方");
+document.writeln("</br>　　　　東台刷人頭,願逝者安息...");
 document.writeln("</br></br>"); //保留換行
