54f2d5d0184fc6467c692253e84e568ac8bda6c6
visionbaby <visionbaby@users.noreply.github.com>
2018-01-26 00:32:59 +0800

    Update text.js

--- js/text.js
+++ js/text.js
@@ -1,3 +1,2 @@
-document.writeln("</br>　大家.下週都乖乖宅家吧,出門會爽死的...");
-document.writeln("</br>　　　　　　　　　　　　　　　20180118");
+document.writeln("</br>　乖乖宅家吧,才不想出門會爽死的...");
 document.writeln("</br></br>"); //保留換行
