b115c81defad3fd746b53788a0e161cb86936536
visionbaby <visionbaby@users.noreply.github.com>
2018-07-04 08:55:33 +0800

    Update jquery.prettyphoto.js

--- prettyphoto/js/jquery.prettyphoto.js
+++ prettyphoto/js/jquery.prettyphoto.js
@@ -90,7 +90,7 @@
 			iframe_markup: '<iframe src ="{path}" width="{width}" height="{height}" frameborder="no"></iframe>',
 			inline_markup: '<div class="pp_inline">{content}</div>',
 			custom_markup: '',
-			social_tools: '<div class="jmqx"><a href="http://www.jmqx.gov.cn" target="_blank">www.jmqx.gov.cn</a></div>' /* html or false to disable */
+			social_tools: '<div class="org"><a href="https://zh.wikipedia.org/wiki/Category:氣象機構" target="_blank">©　All weather org</a></div>' /* html or false to disable */
 		}, pp_settings);
 		
 		// Global variables accessible only by prettyPhoto
