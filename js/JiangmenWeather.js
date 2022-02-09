ba7bbe88c834896ff860595ba6f018f209c143fb
visionbaby <visionbaby@users.noreply.github.com>
2022-02-09 14:17:54 +0800

    Delete js directory

--- js/JiangmenWeather.js
+++
@@ -1,4 +0,0 @@
-document.writeln("<div style=\'width: 475px;margin:0 auto;\'>");
-document.writeln("  <iframe style=\'overflow:hidden;border:none\' allowtransparency=\'true\' width=\'475\' height=\'205\' src=\'https://zh.weather-forecast.com/locations/Jiangmen/forecasts/latest/threedayfree\' scrolling=\'no\' frameborder=\'0\' marginwidth=\'0\' marginheight=\'0\'>");
-document.writeln("  </iframe>");
-document.writeln("</div>");
